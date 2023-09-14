86f83effc6c3597ae9db9f49a8c49de96edd5408
asrafilll <asrafilr@gmail.com>
2023-09-14 22:37:31 +0700

    feat: Setup client connection with api & websocket server

--- src/App.js
+++ src/App.js
@@ -1,35 +1,106 @@
 import React, { useState, useEffect } from "react";
 import DefaultScreen from "./components/DefaultScreen";
+import AlternativeScreen from "./components/AlternativeScreen";
 import DefaultError from "./pages/DefaultError";
+import Pusher from "pusher-js";
+
+const pusherAPPKey = process.env.REACT_APP_PUSHER_KEY;
+const pusherCluster = process.env.REACT_APP_PUSHER_CLUSTER;
+const backendURL = process.env.REACT_APP_BACKEND_URL;
+
+const pusher = new Pusher(pusherAPPKey, {
+  cluster: pusherCluster,
+});
 
 export default function App() {
   const [defaultConfiguration, setDefaultConfiguration] = useState();
   const [alternativeConfiguration, setAlternativeConfiguration] = useState();
   const [loading, setLoading] = useState(true);
+  const [timeoutId, setTimeoutId] = useState();
+  const [activeConfiguration, setActiveConfiguration] = useState();
 
   useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const key = urlParams.get("key");
+    const channel = pusher.subscribe(`screen.${key}`);
+    channel.bind("screen.updated", function (message) {
+      const { default_configuration, alternative_configuration } = message.data;
+      setDefaultConfiguration(default_configuration);
+      setAlternativeConfiguration(alternative_configuration);
+      if (
+        alternative_configuration !== null &&
+        Boolean(alternative_configuration.is_active)
+      ) {
+        clearTimeout(timeoutId);
+        if (alternative_configuration.duration_time > 0) {
+          setTimeoutId(
+            setTimeout(() => {
+              setActiveConfiguration("default");
+            }, alternative_configuration.duration_time * 1000)
+          );
+        }
+        const disableAlternative = async () => {
+          await fetch(
+            `${backendURL}/api/outlet-screens/${key}/alternative-configuration/deactivate`,
+            {
+              method: "POST",
+            }
+          );
+        };
+
+        disableAlternative();
+        setActiveConfiguration("alternative");
+      }
+    });
+    return () => {
+      channel.unbind("screen.updated");
+      pusher.unsubscribe(`screen.${key}`);
+    };
+  }, []);
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const key = urlParams.get("key");
     const getData = async () => {
-      const urlParams = new URLSearchParams(window.location.search);
-      const key = urlParams.get("key");
-      const response = await fetch(
-        `http://13.251.246.148/command-centre/api/outlet-screens/${key}`
-      );
+      const response = await fetch(`${backendURL}/api/outlet-screens/${key}`);
       const { data } = await response.json();
       const { default_configuration, alternative_configuration } = data;
       setDefaultConfiguration(default_configuration);
       setAlternativeConfiguration(alternative_configuration);
+      if (
+        alternative_configuration !== null &&
+        Boolean(alternative_configuration.is_active)
+      ) {
+        clearTimeout(timeoutId);
+        if (alternative_configuration.duration_time > 0) {
+          setTimeoutId(
+            setTimeout(() => {
+              setActiveConfiguration("default");
+            }, alternative_configuration.duration_time * 1000)
+          );
+        }
+
+        setActiveConfiguration("alternative");
+        const disableAlternative = async () => {
+          await fetch(
+            `${backendURL}/api/outlet-screens/${key}/alternative-configuration/deactivate`,
+            {
+              method: "POST",
+            }
+          );
+        };
+        disableAlternative();
+      }
       setLoading(false);
     };
     getData();
   }, []);
 
-  if (loading) {
-    return <DefaultError />;
+  if (loading) return <DefaultError />;
+
+  if (activeConfiguration === "alternative") {
+    return <AlternativeScreen configuration={alternativeConfiguration} />;
   } else {
-    return (
-      <div>
-        <DefaultScreen configuration={defaultConfiguration} />
-      </div>
-    );
+    return <DefaultScreen configuration={defaultConfiguration} />;
   }
 }
---
+++ src/components/AlternativeScreen.js
@@ -0,0 +1,26 @@
+import TopBottomLeftPanel from "../pages/TopBottomLeftPanel";
+import FullScreen from "../pages/FullScreen";
+import TopBottom from "../pages/TopBottom";
+import LeftRight from "../pages/LeftRight";
+import HeaderContentFooter from "../pages/HeaderContentFooter";
+import TopBottomRightPanel from "../pages/TopBottomRightPanel";
+import DefaultError from "../pages/DefaultError";
+
+export default function AlternativeScreen({ configuration }) {
+  switch (+configuration.template) {
+    case 0:
+      return <FullScreen contents={configuration.contents} />;
+    case 1:
+      return <TopBottom contents={configuration.contents} />;
+    case 2:
+      return <LeftRight contents={configuration.contents} />;
+    case 3:
+      return <HeaderContentFooter contents={configuration.contents} />;
+    case 4:
+      return <TopBottomLeftPanel contents={configuration.contents} />;
+    case 5:
+      return <TopBottomRightPanel contents={configuration.contents} />;
+    default:
+      return <DefaultError />;
+  }
+}
